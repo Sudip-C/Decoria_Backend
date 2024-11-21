7e852fdf499bac81bb0e6cf63e67251c90611787
sudip chowdhury <rexalex826@gmail.com>
2024-11-21 09:25:02 +0530

    correction regarding single product route

--- controllers/productController.js
+++ controllers/productController.js
@@ -55,3 +55,24 @@ exports.getFilteredProducts = async (req, res) => {
   }
 };
 
+exports.getSingleProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.isValidObjectId(id)) {
+      return res.status(400).json({ message: 'Invalid product ID' });
+    }
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addProduct, getAllProducts,getFilteredProducts} = require('../controllers/productController');
+const { addProduct, getSingleProduct,getAllProducts,getFilteredProducts} = require('../controllers/productController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/addProduct', addProduct);
 router.get('/getProduct', getAllProducts);
 router.get('/filter', getFilteredProducts);
+router.get('/:id', getSingleProduct);
+
 
 module.exports = router;
